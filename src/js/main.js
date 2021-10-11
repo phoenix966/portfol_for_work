afbc1d418e96c3e779a846a79cafbbe76383fe8b
Uri <stranger354999@gmail.com>
2021-10-11 16:42:24 +0600

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 //=require ../../node_modules/jquery/dist/jquery.js
 
+  let hamburger = document.querySelector(".hamburger");
+  let mobile = document.querySelector(".mobile");
+  
+  hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("is-active");
+    mobile.classList.toggle("mobile--hide");
+  });
 
+  let overlay = document.querySelector('.mobile__overlay');
+  overlay.addEventListener('click',()=>{
+    mobile.classList.toggle('mobile--hide');
+  })
